4e133e8ec9cdb0e41203edf408dddcd471848df0
Konstantinos Antonopoulos <kostas_antonop@hotmail.com>
2025-10-26 10:51:29 +0200

    Initial Commit

---
+++ AssessmentTask/AssessmentTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  AssessmentTaskApp.swift
+//  AssessmentTask
+//
+//  Created by k.a.antonopoulos on 26/10/25.
+//
+
+import SwiftUI
+
+@main
+struct AssessmentTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
