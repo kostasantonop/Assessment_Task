ce72bfe3ef51a78927c81ff86d59d75e91b335d3
Konstantinos Antonopoulos <kostas_antonop@hotmail.com>
2025-10-27 11:54:46 +0200

    Made container to respond to model and state changes

--- AssessmentTask/Foundation/Container.swift
+++ AssessmentTask/Foundation/Container.swift
@@ -12,9 +12,16 @@ public final class Container<Intent, Model>: ObservableObject {
     
     public let intent: Intent
     public let model: Model
+    
+    private var cancellable: Set<AnyCancellable> = []
 
-    public init(intent: Intent, model: Model) {
+    public init(intent: Intent, model: Model, modelChangePublisher: ObjectWillChangePublisher) {
         self.intent = intent
         self.model = model
+        
+        modelChangePublisher
+            .receive(on: RunLoop.main)
+            .sink(receiveValue: objectWillChange.send)
+            .store(in: &cancellable)
      }
 }
